63d6d9da9a9fd1809d85351aba8bb0b4032c0431
Xuan Hui(xuanhui919) <541951523@qq.com>
2020-02-27 18:01:22 +0800

    change load vue ssr config

--- router/routes.js
+++ router/routes.js
@@ -1,20 +1,20 @@
 const fs = require('fs')
-const path = require('path')
+const { resolve } = require('path')
 const Router = require('@koa/router')
 const { createBundleRenderer } = require('vue-server-renderer')
 
-const template = fs.readFileSync(path.resolve(__dirname, '../views/ssr-template.htm'), 'utf-8')
+const template = fs.readFileSync(resolve(__dirname, '../views/ssr-template.htm'), 'utf-8')
 
-module.exports = function ({ location, paths, title }) {
+module.exports = function ({ mountpoint, paths, title }) {
   const router = new Router()
 
   paths.forEach(item => {
-    const clientManifest = require(`${location}/vue-ssr-client-manifest.json`)
+    const clientManifest = require(`../mount/${mountpoint}/vue-ssr-client-manifest.json`)
 
-    const renderer = createBundleRenderer(`${location}/vue-ssr-server-bundle.json`, {
+    const renderer = createBundleRenderer(resolve(__dirname, '../mount', mountpoint, 'vue-ssr-server-bundle.json'), {
       runInNewContext: false, // 推荐
       template, // （可选）页面模板
-      clientManifest// （可选）客户端构建 manifest
+      clientManifest // （可选）客户端构建 manifest,
     })
 
     router.get(item, async ctx => {
